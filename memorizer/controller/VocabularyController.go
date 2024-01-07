90a87c670e094191f32c03e05d5402121e5bb9ba
mukeshdutt <mukeshdutt@outlook.com>
2024-01-07 23:22:14 +0530

    added all the controllers

---
+++ memorizer/controller/VocabularyController.go
@@ -0,0 +1,23 @@
+package controller
+
+import "github.com/gofiber/fiber/v2"
+
+func GetVocabulary(c *fiber.Ctx) error {
+	return c.SendString("")
+}
+
+func GetAllVocabulary(c *fiber.Ctx) error {
+	return c.SendString("")
+}
+
+func AddVocabulary(c *fiber.Ctx) error {
+	return c.SendString("")
+}
+
+func EditVocabulary(c *fiber.Ctx) error {
+	return c.SendString("")
+}
+
+func RemoveVocabulary(c *fiber.Ctx) error {
+	return c.SendString("")
+}
